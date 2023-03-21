3386d4a09d7f04b22c2321b96f54f57d59e4f9da
NguyenVHLan <100670844+NguyenVHLan@users.noreply.github.com>
2023-03-21 12:36:12 +0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Marketplace = artifacts.require("Marketplace");
+const Marketplace = artifacts.require("ProductPricing");
 
 module.exports = function(deployer) {
-  deployer.deploy(Marketplace);
+  deployer.deploy(ProductPricing);
 };
